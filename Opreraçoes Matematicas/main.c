fb422aa4377a461beb19a04508f525cabd154297
Carlos Henrique <97001399+CA4L051327@users.noreply.github.com>
2022-01-02 23:36:03 -0300

    Opreraçoes Matematicas

---
+++ Opreraçoes Matematicas/main.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+  //DEFININDO VARIAVEIS
+  int a = 6, b = 3;
+
+  //SOMA
+printf("\n A soma de  %d   e  %d  =  %d", a ,  b ,  a  +  b);
+
+    //SUBTRAÇAO
+printf("\n A subtracao de  %d   e  %d  =  %d", a ,  b ,  a  -  b);
+
+  //DIVISAO
+printf("\n A divisao de  %d   e  %d  =  %d", a ,  b ,  a  /  b);
+
+  //MULTIPLICAÇAO
+printf("\n A multiplicacao de  %d   e  %d  =  %d", a ,  b ,  a  *  b);
+
+//RESTO DA DIVISAO
+printf("\n O resto da divisao entre  %d   e  %d  =  %d", a ,  b ,  a  %  b);
+
+  //VALOR ABSOLUTO
+  printf("\n O valor absoluto de  -3 = %d" , abs(-3));
+
+//PAUSA O PROGRAMA
+system("pause");
+}
+
