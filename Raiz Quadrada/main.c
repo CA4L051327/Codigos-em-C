7457d6bcbb988dd74669071b04dd8e6388ef4ee8
Carlos Henrique <97001399+CA4L051327@users.noreply.github.com>
2022-01-02 23:37:28 -0300

    Raiz Quadrada

---
+++ Raiz Quadrada/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main()
+{
+  float a=5, b=10, c=-20;
+
+            float r1 = ( -b+ (sqrt(b*b-4*a*c)));      (2*a) ;
+            float r2 = ( -b- (sqrt(b*b-4*a*c)));       (2*a) ;
+
+        printf ("Raiz 1  =  %f\nRaiz  2   =  %f", r1,  r2);
+
+            return 0;
+}
