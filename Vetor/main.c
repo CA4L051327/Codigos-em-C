2f1a76d3728ef9f76918f70483980f3f72f29e42
Carlos Henrique <97001399+CA4L051327@users.noreply.github.com>
2022-01-02 23:52:11 -0300

    Vetor

---
+++ Vetor/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+        int numero [10], i;
+
+        for(i  =  0; i   <  10;    i++){
+            printf("Digite um numero\n");
+            scanf("%", &numero [i]);
+            }
+
+            for(i = 0;  i  <  10;   i++); {
+                printf("O numero eh %i\n", numero [i]);
+            }
+
+            return 0;
+
+
+}
